c3708a63f77f1fe787be22f155cd382a367c2f29
Nanda <tynandu@gmail.com>
2023-07-25 07:11:34 +0530

    Solution for leetcode #1678, approach 2

---
+++ 1678-Goal-Parser-Interpretation/solution_2.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string interpret(string command) {
+        while(command.find("()") != string::npos) command.replace(command.find("()"), 2, "o");
+        while(command.find("(al)") != string::npos) command.replace(command.find("(al)"), 4, "al");
+        return command;
+    }
+};
