a6ae82f0e034cd2ed1a704ac2c0e1469f65c3d34
Nanda <tynandu@gmail.com>
2023-08-03 20:46:52 +0530

    Solution for leetcode problem #1512

---
+++ 1512-Number-of-Good-Pairs/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int, int> countMap;
+
+        for(const auto& num : nums) {
+            ++countMap[num];
+        }
+
+        int result = 0;
+        for(const auto&[num, count] : countMap) {
+            if(count > 1) {
+                result += (count * (count - 1)) / 2;
+            }
+        }
+
+        return result;
+    }
+};
