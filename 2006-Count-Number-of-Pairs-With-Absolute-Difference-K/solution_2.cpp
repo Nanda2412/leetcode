715e1e478342fb57b892f2017f1a20aae1b9ddc6
Nanda <tynandu@gmail.com>
2023-08-18 19:44:14 +0530

    Solution for leetcode problem #2006, approach 2

---
+++ 2006-Count-Number-of-Pairs-With-Absolute-Difference-K/solution_2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int result = 0;
+        unordered_map<int,int> countMap;
+
+        const static auto& countKDiffPairs = [&](const auto& num) {
+            if(countMap.count(abs(num - k))) {
+                result += countMap[num - k];
+            } 
+            if(countMap.count(num + k)) {
+                result += countMap[num + k];
+            }
+
+             ++countMap[num];
+        };
+
+        for_each(nums.begin(), nums.end(), countKDiffPairs);
+
+        return result;
+    }
+};
