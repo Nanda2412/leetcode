373873807850a62db7b35413042f867042346831
Nanda <tynandu@gmail.com>
2023-09-09 21:49:42 +0530

    Solution for leetcode problem #2341, alternate approach

---
+++ 2341-Maximum-Number-of-Pairs-in-Array/solution_1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        unordered_set<int> uniqueNums;
+
+        int pairsFormed = 0;
+        for(const auto& num : nums) {
+            if(uniqueNums.count(num)) {
+                ++pairsFormed;
+                uniqueNums.erase(num);
+                continue;
+            }
+
+            uniqueNums.emplace(num);
+        }
+
+        return vector<int>{pairsFormed, static_cast<int>(uniqueNums.size())};
+    }
+};
