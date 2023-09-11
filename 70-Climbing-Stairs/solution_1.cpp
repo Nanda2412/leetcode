9a6a08bf2fd027c73da60fcb4897db5dfc113d07
Nanda <tynandu@gmail.com>
2023-09-11 08:09:37 +0530

    Updated solution_1.cpp

--- 70-Climbing-Stairs/solution_1.cpp
+++ 70-Climbing-Stairs/solution_1.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 1) return 1;
-        vector<int> numSteps(n + 1, 0);
-        numSteps[1] = 1, numSteps[2] = 2;
-        for(int i = 3; i <= n; ++i) {
+        vector<int> numSteps(n + 1, 1);
+        for(int i = 2; i <= n; ++i) {
             numSteps[i] = numSteps[i - 1] + numSteps[i - 2];
         }
 
