3d6151d2f240eaf567b55155a21f466465e78c85
Nanda <tynandu@gmail.com>
2023-09-19 12:11:48 +0530

    Updated solution_1.cpp

--- 2496-Maximum-Value-of-a-String-in-an-Array/solution_1.cpp
+++ 2496-Maximum-Value-of-a-String-in-an-Array/solution_1.cpp
@@ -3,11 +3,8 @@ public:
     int maximumValue(vector<string>& strs) {
         int result = 0;
 
-        const auto isDigit = [](const char& c) {
-            return isdigit(c);
-        };
         for(const auto& str : strs) {
-            if(all_of(str.begin(), str.end(), isDigit)) {
+            if(all_of(str.begin(), str.end(), ::isdigit)) {
                 result = max(result, stoi(str));
             } else {
                 result = max(result, static_cast<int>(str.size()));
