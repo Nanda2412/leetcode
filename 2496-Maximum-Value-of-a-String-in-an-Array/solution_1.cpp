9645bb07faa12bab58a9b3ed40a41c4d4218e926
Nanda <tynandu@gmail.com>
2023-09-19 11:48:46 +0530

    Updated solution_1.cpp

--- 2496-Maximum-Value-of-a-String-in-an-Array/solution_1.cpp
+++ 2496-Maximum-Value-of-a-String-in-an-Array/solution_1.cpp
@@ -8,14 +8,7 @@ public:
         };
         for(const auto& str : strs) {
             if(all_of(str.begin(), str.end(), isDigit)) {
-                if(str.find_first_not_of('0') == string::npos) continue;
-
-                int num = 0;
-                for(const auto& c : str) {
-                    num = num * 10 + c - '0';
-                }
-
-                result = max(result, num);
+                result = max(result, stoi(str));
             } else {
                 result = max(result, static_cast<int>(str.size()));
             }
