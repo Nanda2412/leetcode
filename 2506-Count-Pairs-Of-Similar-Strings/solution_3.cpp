4e9256fef35cbf9434688950c6e14bbb64cc9559
Nanda <tynandu@gmail.com>
2023-09-30 12:31:41 +0530

    Solution for leetcode problem #2506, approach 3

---
+++ 2506-Count-Pairs-Of-Similar-Strings/solution_3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    using ulong = unsigned long;
+public:
+    int similarPairs(vector<string>& words) {
+        unordered_map<ulong, size_t> countMap;
+        for(auto& word : words) {
+            bitset<26> bits;
+            for(const auto& c : word) {
+                bits[c - '0'] = true;
+            }
+
+            ++countMap[bits.to_ulong()];
+        }
+
+        int result = 0;
+        for(const auto&[_, count] : countMap) {
+            result += ( count * (count - 1) ) / 2;
+        }
+
+        return result;
+    }
+};
