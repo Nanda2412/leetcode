07b42c3362a0ee8c44e4628128cf04e995c98540
Nanda <tynandu@gmail.com>
2023-10-14 22:55:27 +0530

    Updated solution_1.cpp

--- 697-Degree-of-an-Array/solution_1.cpp
+++ 697-Degree-of-an-Array/solution_1.cpp
@@ -1,14 +1,14 @@
 class Solution { 
 public:
     int findShortestSubArray(vector<int>& nums) {
-        unordered_map<int, set<int>> indexMap;
+        unordered_map<int, vector<int>> indexMap;
 
         int degree = 0;
         for(int i = 0; i < nums.size(); ++i) {
             if(indexMap.count(nums[i])) {
-                indexMap[nums[i]].emplace(i);
+                indexMap[nums[i]].emplace_back(i);
             } else {
-                indexMap[nums[i]] = set<int>{i};
+                indexMap[nums[i]] = vector<int>{i};
             }
 
             degree = max(degree, static_cast<int>(indexMap[nums[i]].size()));
@@ -17,7 +17,7 @@ public:
         int result = INT_MAX;
         ranges::for_each(indexMap, [&](const auto& p){
             if(degree == p.second.size()) {
-                result = min(result, *p.second.rbegin() - *p.second.begin() + 1);
+                result = min(result, p.second.back() - p.second.front() + 1);
             }
         });
 
