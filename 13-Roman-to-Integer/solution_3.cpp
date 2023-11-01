8a25def866cb87890a99df0df552af1dc31a41a8
Nanda <tynandu@gmail.com>
2023-11-01 23:13:35 +0530

    Solution for leetcode problem #13, approach 3

---
+++ 13-Roman-to-Integer/solution_3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int result = 0;
+        for(int i = s.size() - 1; i >= 0; --i) {
+            switch(s[i]) {
+                case 'I':
+                    if(result >= 5) result -= 1;
+                    else result += 1;
+                    break;
+                case 'V':
+                    result += 5;
+                    break;
+                case 'X':
+                    if(result >= 50) result -= 10;
+                    else result += 10;
+                    break;
+                case 'L':
+                    result += 50;
+                    break;
+                case 'C':
+                    if(result >= 500) result -= 100;
+                    else result += 100;
+                    break;
+                case 'D':
+                    result += 500;
+                    break;
+                case 'M':
+                    result += 1000;
+                    break;
+            }
+        }
+
+        return result;
+    }
+};
