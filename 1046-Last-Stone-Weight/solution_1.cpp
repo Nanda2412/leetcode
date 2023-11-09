c9ab05728c9977e3c9f5fe7dfd26b676408f4dbd
Nanda <tynandu@gmail.com>
2023-11-09 12:51:28 +0530

    Updated solution_1.cpp

--- 1046-Last-Stone-Weight/solution_1.cpp
+++ 1046-Last-Stone-Weight/solution_1.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-        ranges::sort(stones);
-
         while(stones.size() > 1) {
+            ranges::sort(stones);
             const auto first = stones.back(); stones.pop_back();
             const auto second = stones.back(); stones.pop_back();
             stones.emplace_back(first - second);
-            ranges::sort(stones);
         }
 
         return stones.front();
