d2c40e87cf3e61492c11deb0e59a877c0f80ed4a
Nanda <tynandu@gmail.com>
2023-11-15 20:16:22 +0530

    Solution for leetcode problem #1346, approach 1

---
+++ 1346-Check-If-N-and-Its-Double-Exist/solution_1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        
+        for(size_t i = 0; i < arr.size(); ++i) {
+            for(size_t j = i + 1; j < arr.size(); ++j) {
+                if(arr[i] == 2 * arr[j] || arr[j] == 2 * arr[i]) return true;
+            }
+        }
+
+        return false;
+    }
+};
